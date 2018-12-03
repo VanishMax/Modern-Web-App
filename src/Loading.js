b241c10480dcaf37f8c528152759c006e4a38678
VanishMax <maxkors2014@gmail.com>
2018-12-03 19:27:42 +0300

    Update Loading.js

--- src/Loading.js
+++ src/Loading.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import CircularProgress from '@material-ui/core/CircularProgress'
+import Snackbar from '@material-ui/core/Snackbar'
+import Button from '@material-ui/core/Button'
 
 export default function Loading(props) {
   if (props.error) {
-    return <div>Error! <button onClick={ props.retry }>Retry</button></div>
+    return <Snackbar
+      anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+      open={props.error} autoHideDuration={null}
+      ContentProps={{ 'aria-describedby': 'message-id' }}
+      message={<span id="message-id">Please, refresh the page</span>}
+      action={[
+        <Button key="undo" color="secondary" onClick={ () => window.location.reload(true) }>
+          Refresh
+        </Button>
+      ]}
+    />
   } else if (props.pastDelay) {
     return <CircularProgress color="primary"/>
   } else {
