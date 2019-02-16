da32a5e0020d4d867bf56e3689428912f55889dd
victorkolb <violetmonox@gmail.com>
2019-02-16 22:08:10 +0300

    app routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,26 +1,13 @@
-import React from 'react'
-import { Switch, Route } from 'react-router'
-
-
-import Loadable from 'react-loadable'
-import Loading from '~/src/Loading'
-const AsyncHome = Loadable({
-  loader: () => import(/* webpackChunkName: "Home" */ './Home'),
-  loading: Loading,
-  delay: 300,
-})
-const AsyncAbout = Loadable({
-  loader: () => import(/* webpackChunkName: "About" */ './About'),
-  loading: Loading,
-  delay: 300,
-})
-
+import React from "react";
+import { Switch, Route } from "react-router";
+import routes from "./routes";
 
 export default function App() {
-  return(
+  return (
     <Switch>
-      <Route exact path="/" component={AsyncHome}/>
-      <Route exact path="/about" component={AsyncAbout}/>
+      {routes.map(route => (
+        <Route key={route.path} {...route} />
+      ))}
     </Switch>
-  )
+  );
 }
